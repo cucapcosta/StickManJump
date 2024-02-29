03c9bb5036d8543beeafe0eee84daf0ef6668139
cucapcosta <lucas.costa@sou.inteli.edu.br>
2024-02-29 08:02:54 -0300

    Update intro.js

--- scripts/intro.js
+++ scripts/intro.js
@@ -5,9 +5,9 @@ class StartScene extends Phaser.Scene{
     }
 
     preload(){
-        this.load.image('bgazul', 'StickManJump/assets/fundo.png');
-        this.load.image('botao', 'StickManJump/assets/play.png');
-        this.load.image('logo', 'StickManJump/assets/logo.png');
+        this.load.image('bgazul', 'assets/fundo.png');
+        this.load.image('botao', 'assets/play.png');
+        this.load.image('logo', 'assets/logo.png');
     }
     
     create(){
